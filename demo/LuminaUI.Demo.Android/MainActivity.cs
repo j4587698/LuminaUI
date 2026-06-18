479e605533a9bbfe8f5d9420e09cfd757805fe70
j4587698 <jx@jvxiang.com>
2026-06-18 16:42:42 +0800

    feat: add mobile brand assets

--- demo/LuminaUI.Demo.Android/MainActivity.cs
+++ demo/LuminaUI.Demo.Android/MainActivity.cs
@@ -1,15 +1,25 @@
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Android.Views;
+using AndroidX.Core.SplashScreen;
 using Avalonia.Android;
 
 namespace LuminaUI.Demo.Android;
 
 [Activity(
     Label = "LuminaUI Demo",
-    Theme = "@style/LuminaUI.Theme",
+    Icon = "@mipmap/appicon",
+    Theme = "@style/LuminaUI.LaunchTheme",
     MainLauncher = true,
     LaunchMode = LaunchMode.SingleTop,
     WindowSoftInputMode = SoftInput.AdjustNothing,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity;
+public class MainActivity : AvaloniaMainActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        SplashScreen.InstallSplashScreen(this);
+        base.OnCreate(savedInstanceState);
+    }
+}
